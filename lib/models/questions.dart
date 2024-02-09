2b587cda9fed69e3b51cda81ed177e0fb565edb6
Penpitcha22 <penpitcha306@gmail.com>
2024-02-09 19:09:17 +0700

    first commit

---
+++ lib/models/questions.dart
@@ -0,0 +1,105 @@
+import 'package:quiz_app_tutorial/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. จุลินทรีย์มีกี่ชนิด',
+    correctAnswerIndex: 3,
+    options: [
+      'a) 5 ชนิด',
+      'b) 4 ชนิด',
+      'c) 3 ชนิด',
+      'd) 2 ชนิด',
+    ],
+  ),
+  Question(
+    question: '2. หม้อนึ่งฆ่าเชื้อความดันไอ(autoclave)ใช้ความดัน',
+    correctAnswerIndex: 2,
+    options: [
+      'a) 15 ปอนด์',
+      'b) 17 ปอนด์',
+      'c) 19ปอนด์',
+      'd) 21ปอนด์',
+    ],
+  ),
+  Question(
+    question: '3. จานเพาะเชื้อทำให้ปราศจากเชื้อโดยวิธีใด',
+    correctAnswerIndex: 3,
+    options: [
+      'a) นำไปเผาผ่านไฟ',
+      'b) นำไปต้มในนำ้เดือด',
+      'c) นำไปอบในตู้อบลมร้อน',
+      'd) นำไปใส่หม้อนึ่งฆ่าเชื้อความดันไอ',
+    ],
+  ),
+  Question(
+    question: '4. อาหารเลี้ยงเชื้อ PDA ใช้เลี้ยงจุลินทรีย์ชนิดใด',
+    correctAnswerIndex: 0,
+    options: [
+      'a) เเบคทีเรีย',
+      'b) รา',
+      'c) ยีสต์',
+      'd) ไวรัส',
+    ],
+  ),
+  Question(
+    question: '5.ข้อใดไม่ใช่องค์ประกอบหลักของเซลล์จุลินทรีย์',
+    correctAnswerIndex: 2,
+    options: [
+      'a) เเหล่งคาร์บอน ',
+      'b) ไนโตรเจน',
+      'c) ธาตุอาหารรอง',
+      'd) สังกะสี',
+    ],
+  ),
+  Question(
+    question: '6. เเบคทีเรียเจริญได้ดีในค่า pHที่เท่าไหร่',
+    correctAnswerIndex: 4,
+    options: [
+      'a) pH 4',
+      'b) pH 5',
+      'c) pH 6',
+      'd) pH 7',
+    ],
+  ),
+  Question(
+    question: '7. ขั้นตอนเเรกก่อนทำการเตรียมอาหารเลี้ยงเชื้อคืออะไร',
+    correctAnswerIndex: 0,
+    options: [
+      'a) ชั่งอาหารเลี้ยงเชื้อใส่ขวดดูเเรน',
+      'b) นำไปฆ่าเชื้อด้วยautoclave',
+      'c) คำนวณปริมาณอาหารเลี้ยงเชื้อ',
+      'd) เทอาหารลงจานเพาะเชื้อ',
+    ],
+  ),
+  Question(
+    question: '8. ราสกุลไหนที่ใช้ในการทำเต้าหู้ยี้',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Mucor',
+      'b) Rhizopus',
+      'c) Aspergillus',
+      'd) Penicillium',
+    ],
+  ),
+  Question(
+    question:
+        '9. เอนไซม์ใดทำหน้าที่เร่งปฏิกิริยาการเกิดสีน้ำตาลของผักผลไม้',
+    correctAnswerIndex: 4,
+    options: [
+      'a) OPP',
+      'b) APO',
+      'c) PPO',
+      'd) POP',
+    ],
+  ),
+  Question(
+    question: '10. เเคโรทีนอยด์เป็นรงควัตถุสีอะไร',
+    correctAnswerIndex: 2,
+    options: [
+      'a) ม่วง',
+      'b) ส้ม',
+      'c) เหลือง',
+      'd) เขียว',
+    ],
+  ),
+];
